355cb0ad11ee7855258a2587716e1b68ffaffb5c
ovidiupetric99 <ovidiupetric99@gmail.com>
2022-05-12 23:30:34 +0300

    updating directory page with directory component and category-item component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from './components/directory/directory.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const categories = [
+    {
+      title: 'create new user',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS60-b4CLmyxCYJfhqmN1nLhVh4PACmIzU9qg7spaVoRQ9KK566yGew-pXN1dwLo6GF30g&usqp=CAU',
+      id: 1,
+      linkUrl: 'create-new-user',
+    },
+    {
+      title: 'switch user',
+      imageUrl: 'https://media.istockphoto.com/vectors/update-profile-personal-account-icon-change-avatar-reset-settings-vector-id925175888?k=20&m=925175888&s=612x612&w=0&h=uXqQIFqr2vQdEZlgjZesclJA3yXvbnb9n-J_23C96Cw=',
+      id: 2,
+      linkUrl: '',
+    },
+    {
+      title: 'edit user',
+      imageUrl: 'https://www.iconbolt.com/preview/twitter/font-awesome-solid/user-edit.svg',
+      id: 3,
+      linkUrl: '',
+    },
+    {
+      title: 'view diet',
+      imageUrl: 'https://images.everydayhealth.com/images/what-is-the-dash-diet-1440x810.jpg',
+      size: 'large',
+      id: 4,
+      linkUrl: '',
+    },
+    {
+      title: 'user data',
+      imageUrl: 'https://image.shutterstock.com/image-vector/account-settings-line-icon-260nw-1140725783.jpg',
+      size: 'large',
+      id: 5,
+      linkUrl: '',
+    },
+  ];
+
+  return <Directory categories={categories} />;
+};
 
 export default App;
